f73e670c6f7a8b4686fe4cbadbb4205c7f7088bc
mrfi7 <muh.rifai@megafinance.co.id>
2021-12-19 18:40:41 +0700

    update

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,36 +1,11 @@
-import React from "react";
-import Document, { Head, Main, NextScript } from "next/document";
+import Document, { DocumentContext } from 'next/document'
 
 class MyDocument extends Document {
-  render() {
-    return (
-      <html lang="en">
-        <Head>
-          <meta charSet="utf-8" />
-          <meta name="theme-color" content="#000000" />
-          <link
-            rel="shortcut icon"
-            href={require("assets/img/brand/favicon.ico")}
-          />
-          <link
-            rel="apple-touch-icon"
-            sizes="76x76"
-            href={require("assets/img/brand/apple-icon.png")}
-          />
-          {/* Fonts and icons */}
-          <link
-            href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700&display=optional"
-            rel="stylesheet"
-          />
-        </Head>
-        <body>
-          <div id="page-transition"></div>
-          <Main />
-          <NextScript />
-        </body>
-      </html>
-    );
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx)
+
+    return initialProps
   }
 }
 
-export default MyDocument;
+export default MyDocument
